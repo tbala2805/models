d65db0e526da321897cb508ec2973121e2adf006
ankita84179 <78857399+ankita84179@users.noreply.github.com>
2021-02-15 22:18:52 +0530

    Update export_model.py

--- research/deeplab/export_model.py
+++ research/deeplab/export_model.py
@@ -30,7 +30,7 @@ VH_INPUTS_DIR = os.getenv('VH_INPUTS_DIR')
 FLAGS = flags.FLAGS
 
 # flags.DEFINE_string('checkpoint_path', '/valohai/inputs/Logs/model.ckpt-100', 'Checkpoint path')
-checkpoint_path = os.path.join(VH_INPUTS_DIR, checkpoint-path)
+checkpoint_path = os.path.join(VH_INPUTS_DIR, 'checkpoint-path','model.ckpt-100')
 flags.DEFINE_string('export_path', '/valohai/outputs/frozen_inference_graph.pb',
                     'Path to output Tensorflow frozen graph.')
 
@@ -75,7 +75,7 @@ _RAW_OUTPUT_NAME = 'RawSemanticPredictions'
 # Output name of the exported probabilities.
 _OUTPUT_PROB_NAME = 'SemanticProbabilities'
 _RAW_OUTPUT_PROB_NAME = 'RawSemanticProbabilities'
-image_folder = os.path.join(VH_INPUTS_DIR, 'Checkpoint-path','model.ckpt-100')
+# image_folder = os.path.join(VH_INPUTS_DIR, 'Checkpoint-path','model.ckpt-100')
 
 def _create_input_tensors():
   """Creates and prepares input tensors for DeepLab model.
